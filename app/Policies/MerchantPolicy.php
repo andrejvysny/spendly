782fa531ca50d59460a78a83f8d5c147beed28ff
Andrej Vyšný <92629581+andrejvysny@users.noreply.github.com>
2025-06-15 00:19:49 +0200

    refactor: extract reusable code

--- app/Policies/MerchantPolicy.php
+++ app/Policies/MerchantPolicy.php
@@ -4,49 +4,8 @@ namespace App\Policies;
 
 use App\Models\Merchant;
 use App\Models\User;
-use Illuminate\Auth\Access\HandlesAuthorization;
 
-class MerchantPolicy
+class MerchantPolicy extends OwnedByUserPolicy
 {
-    use HandlesAuthorization;
-
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        return true;
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Merchant $merchant): bool
-    {
-        return $user->id === $merchant->user_id;
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        return true;
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Merchant $merchant): bool
-    {
-        return $user->id === $merchant->user_id;
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Merchant $merchant): bool
-    {
-        return $user->id === $merchant->user_id;
-    }
+    // Additional merchant-specific authorization logic can be placed here
 }
